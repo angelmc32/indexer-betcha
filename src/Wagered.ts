c02d98d94b9597cc705b07b7da7ff1bf52d53b67
Mel <angel.melendez.c@gmail.com>
2023-09-24 06:33:31 -0400

    Add missing variables

--- src/Wagered.ts
+++ src/Wagered.ts
@@ -1,7 +1,25 @@
 import { ponder } from "@/generated";
+import { createPublicClient, getContract, http } from "viem";
+import { base } from "viem/chains";
+import BetchaRoundABI from "./ABI/BetchaRoundABI";
+
+const client = createPublicClient({
+  chain: base,
+  transport: http(),
+});
 
 ponder.on("BetchaRound/v1/Wagered:Wagered", async ({ event, context }) => {
   const { WageredEvent } = context.entities;
+  const emitterAddress = event.log.address;
+
+  const contract = getContract({
+    address: emitterAddress,
+    abi: BetchaRoundABI,
+    publicClient: client,
+  });
+
+  const metadataURI = await contract.read.metadataURI();
+  const settlementAvailableAt = await contract.read.settlementAvailableAt();
 
   await WageredEvent.create({
     id: `${event.transaction.hash}-${event.log.logIndex}`,
@@ -10,6 +28,8 @@ ponder.on("BetchaRound/v1/Wagered:Wagered", async ({ event, context }) => {
       tokenAddress: event.params.tokenAddress,
       amount: event.params.amount,
       contractAddress: event.log.address,
+      metadataURI: metadataURI,
+      settlementAvailableAt: settlementAvailableAt,
     },
   });
 });
