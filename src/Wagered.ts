6c5f0d225610a34600d10ef063e87b02ca8a98d0
Mel <angel.melendez.c@gmail.com>
2023-09-24 04:36:10 -0400

    Fix event id

--- src/Wagered.ts
+++ src/Wagered.ts
@@ -4,7 +4,7 @@ ponder.on("BetchaRound/v1/Wagered:Wagered", async ({ event, context }) => {
   const { WageredEvent } = context.entities;
 
   await WageredEvent.create({
-    id: event.params.tokenAddress,
+    id: `${event.transaction.hash}-${event.log.logIndex}`,
     data: {
       gambler: event.params.gambler,
       tokenAddress: event.params.tokenAddress,
